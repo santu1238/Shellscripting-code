786b58b1617d8d8847ccd9ec861e30aa3ae8d8d8
root <root@ip-10-0-12-106.ec2.internal>
2024-07-27 10:50:00 +0000

    2 numbers version1

---
+++ addition.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Calculate the sum of two integers with pre initialize values
+# in a shell script
+
+# Initialize two variables with number
+a=10
+b=20
+
+# Calculate sum
+sum=$(( $a + $b ))
+
+# Display the result
+echo "Sum is: $sum"
+
